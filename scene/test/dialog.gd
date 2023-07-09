1edea1de80ee7f1e4e79bef94e0c33734b32aa2d
SimonPiCarter <xx@gmail.com>
2023-07-09 09:41:20 +0200

    added dialog singal and speed up

--- scene/test/dialog.gd
+++ scene/test/dialog.gd
@@ -6,33 +6,51 @@ extends Node2D
 
 var reading : bool = false
 var muted : bool = false
+var over : bool = false
 
 # character per second
 var reading_speed : int = 20
 var read_step : float = 0
 
+signal is_over()
+
 func read():
 	var rand = RandomNumberGenerator.new()
 	reading = true
+	read_step = 0
+	over = false
 	if not muted:
 		voice.play(rand.randf_range(0, 10.))
+	speed_up(false)
 
 func mute():
 	muted = true
 	voice.stop()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func speed_up(speed : bool = true):
+	if speed:
+		reading_speed = 200
+		voice.pitch_scale = 2
+	else:
+		reading_speed = 20
+		voice.pitch_scale = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not reading:
 		return
 
-	read_step += delta
+	read_step += delta * reading_speed
 
-	text.visible_characters = read_step * reading_speed
-	
+	text.visible_characters = read_step
+
 	if text.visible_characters >= text.text.length():
 		voice.stop()
+		over = true
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		if over:
+			is_over.emit()
+		elif reading:
+			speed_up()
--- scene/test/test_dialog.gd
+++ scene/test/test_dialog.gd
@@ -4,7 +4,4 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$dialog.read()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$dialog.is_over.connect($dialog.read)
